b3794c6733335281f34db40271b3b6188b5417b4
Xincap <xincap@yeah.net>
2016-01-05 12:51:02 +0800

    update

---
+++ src/Http/Controllers/WechatController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Xincap\Wechat\Http\Controllers;
+
+use Ue\Http\Controllers\Controller;
+use Overtrue\Wechat\Server;
+use Overtrue\Wechat\Message;
+use Request;
+use Ue\Model\Wechat;
+use Xincap\Wechat\Plugin\Loader;
+
+class WechatController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+
+        $appId = Request::get('app_id', 123);
+
+        $wechat = Wechat::where(['app_id' => $appId])->first();
+        
+        if (!is_object($wechat)) {
+            return response('user not found.');
+        }
+
+        $server = new Server($wechat->app_id, $wechat->token, $wechat->des_key);
+        
+        $server->on('message', function($message) use ($wechat) {
+            return Loader::proccess($wechat, $message);
+        });
+        
+        return $server->serve();
+    }
+
+}
--- src/Plugin/Loader.php
+++ src/Plugin/Loader.php
@@ -3,29 +3,55 @@
 namespace Xincap\Wechat\Plugin;
 
 use Overtrue\Wechat\Message;
+use Ue\Model\Wechat;
+use DB;
+use Log;
 
 class Loader {
+    
+    private static $fun = [];
+    
+    private static function plugins() {
+        $plugins = [];
+        $plugins['kong_qi'] = \Xincap\Wechat\Plugin\KongQi::class;
+        $plugins['jie_meng'] = \Xincap\Wechat\Plugin\JieMeng::class;
+        $plugins['mobile_region'] = \Xincap\Wechat\Plugin\MobileRegion::class;
+        $plugins['train'] = \Xincap\Wechat\Plugin\Train::class;
+        $plugins['flight'] = \Xincap\Wechat\Plugin\Flight::class;
+        $plugins['dream'] = \Xincap\Wechat\Plugin\Dream::class;
+        $plugins['joke'] = \Xincap\Wechat\Plugin\Joke::class;
+        $plugins['game'] = \Xincap\Wechat\Plugin\Game::class;
+        $plugins['chat'] = \Xincap\Wechat\Plugin\Chat::class;
+        return $plugins;
+    }
 
-    private static $plugins = [
-        \Xincap\Wechat\Plugin\KongQi::class,
-        \Xincap\Wechat\Plugin\JieMeng::class,
-        \Xincap\Wechat\Plugin\MobileRegion::class,
-        \Xincap\Wechat\Plugin\Train::class,
-        \Xincap\Wechat\Plugin\Flight::class,
-        \Xincap\Wechat\Plugin\Dream::class,
-        \Xincap\Wechat\Plugin\Joke::class,
-        \Xincap\Wechat\Plugin\Game::class,
-        \Xincap\Wechat\Plugin\Chat::class,
-    ];
-
-    public static function run($wechat, $message) {
-        foreach (self::$plugins as $key => $plugin) {
-            $s = (new $plugin($wechat, $message));
-            if ($s->getResult()) {
-                return $s->getResult();
+    public static function proccess(Wechat $wechat, $message){
+        
+        DB::enableQueryLog();
+        
+        $wx = Wechat::find(1);
+        
+        $plugins    = $wx->plugins;
+        if(!count($plugins)){
+            return Message::make('text')->content('功能尚未开启.');
+        }
+        
+        foreach ($plugins as $key => $plugin) {
+            self::$fun[]  =  $plugin->name;
+        }
+        
+        $list   = self::plugins();
+        
+        foreach ($list as $key => $plugin) {
+            if(array_key_exists($key, self::$fun)){
+                $fun    = self::$fun[$key];
+                $s = (new $fun($wechat, $message));
+                if ($s->getResult()) {
+                    return $s->getResult();
+                }
             }
         }
         return Message::make('text')->content('维护中.');
     }
-
+    
 }
